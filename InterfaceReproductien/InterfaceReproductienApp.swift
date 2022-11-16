64fd60065b4fae2bea912aba4b3df63afa773d79
Emanuel Gross von Trockau <egrossvontrockau@lcs.on.ca>
2022-11-16 11:48:06 -0500

    Initial Commit

---
+++ InterfaceReproductien/InterfaceReproductienApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceReproductienApp.swift
+//  InterfaceReproductien
+//
+//  Created by Emanuel Gross von Trockau on 2022-11-16.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceReproductienApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
